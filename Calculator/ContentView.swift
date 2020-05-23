151a607493477b1eabd6662eb09d49d1b702fef9
zero0dayali <officialali35@gmail.com>
2020-05-23 16:48:22 +0700

    HStack,VStack,ZStack and funct

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -1,16 +1,40 @@
-//
-//  ContentView.swift
-//  Calculator
-//
-//  Created by Ali on 22/05/20.
-//  Copyright © 2020 Ali. All rights reserved.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    let buttons = [
+        ["7","8","9","X"],
+        ["4","5","6","X"],
+        ["1","2","3","X"],
+        ["0",".",".","X"] 
+    ]
     var body: some View {
-        Text("Hello, World!")
+        ZStack(alignment: .bottom) {
+            Color.black.edgesIgnoringSafeArea(.all)
+            VStack(spacing:12){
+                HStack {
+                    Spacer()
+                    Text("42")
+                        .font(.system(size: 72 ))
+                        .foregroundColor(.white)
+                    .padding()
+                }
+                ForEach(buttons,id: \.self) { row in
+                    HStack {
+                        ForEach(row,id: \.self) { button in
+                            Text(button)
+                                .font(.system(size:32))
+                                .frame(width:self.lebarTombol(),height:self.lebarTombol())
+                                .foregroundColor(.white)
+                                .background(Color.yellow)
+                                .cornerRadius(45)
+                        }
+                    }
+                }
+            }.padding(.bottom)
+        }
+    }
+    func lebarTombol() -> CGFloat {
+        return (UIScreen.main.bounds.width - 5 * 12) / 4
     }
 }
 
